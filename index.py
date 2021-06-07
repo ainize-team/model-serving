1e2adf2f46b20d85241b202788c0e13a947af197
Eunji Lee <eunji@comcom.ai>
2021-06-07 18:34:48 +0900

    private key to env var

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
-import requests
-import json
+import requests, json, os
 from firebase_util import init_firebase, init_model
 from util import *
+
+private_key_id = os.environ['PRIVATE_KEY_ID']
+private_key = os.environ['PRIVATE_KEY']
 
 app = Flask(__name__)
 
@@ -19,6 +21,13 @@ def main():
     print(e)
 
 if __name__ == '__main__':
+  with open('keys/mlops-crawler-firebase-adminsdk.json', 'r') as f:
+    key = json.load(f)
+    print(private_key_id)
+    key['private_key_id'] = private_key_id
+    key['private_key'] = private_key
+    with open('keys/mlops-crawler-firebase-adminsdk.json', 'w', encoding='utf-8') as k:
+      json.dump(key,k)
   init_firebase()
   init_model()
   app.run(host="0.0.0.0", port="5000")
