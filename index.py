0afc3da7c483df191d849f6b6257ec911db82d09
YoungJae <byeongal@comcom.ai>
2021-06-15 14:03:28 +0900

    fix score and black all code

--- index.py
+++ index.py
@@ -1,48 +1,52 @@
 from flask import Flask, request, jsonify
-import requests, json, os
+import requests
+import json
+import os
 from firebase_util import init_firebase, init_model
 from util import *
 
-private_key_id = os.environ['PRIVATE_KEY_ID']
-private_key = os.environ['PRIVATE_KEY']
-service_type = os.environ['ACCOUNT']
-project_id = os.environ['PROJECT_ID']
-client_email = os.environ['CLIENT_EMAIL']
-client_id = os.environ['CLIENT_ID']
-auth_uri = os.environ['AUTH_URI']
-token_uri = os.environ['TOKEN_URI']
-auth_provider = os.environ['AUTH_PROVIDER']
-client = os.environ['CLIENT']
+private_key_id = os.environ["PRIVATE_KEY_ID"]
+private_key = os.environ["PRIVATE_KEY"]
+service_type = os.environ["ACCOUNT"]
+project_id = os.environ["PROJECT_ID"]
+client_email = os.environ["CLIENT_EMAIL"]
+client_id = os.environ["CLIENT_ID"]
+auth_uri = os.environ["AUTH_URI"]
+token_uri = os.environ["TOKEN_URI"]
+auth_provider = os.environ["AUTH_PROVIDER"]
+client = os.environ["CLIENT"]
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def main():
-  data = request.get_json()
-  sequence = data['sequence']
-  try:
-    model, tokenizer = load_model('/workspace/model')
-    result = predict(model, tokenizer, sequence)
-    print(result)
-    return jsonify(result)
-  except Exception as e:
-    print(e)
+    data = request.get_json()
+    sequence = data["sequence"]
+    try:
+        model, tokenizer = load_model("/workspace/model")
+        result = predict(model, tokenizer, sequence)
+        print(result)
+        return jsonify(result)
+    except Exception as e:
+        print(e)
 
-if __name__ == '__main__':
-  key = {}
-  key['private_key_id'] = private_key_id
-  key['private_key'] = private_key
-  key['type'] = service_type
-  key['project_id'] = project_id
-  key['client_email'] = client_email
-  key['client_id'] = client_id
-  key['auth_uri'] = auth_uri
-  key['token_uri'] = token_uri
-  key['auth_provider_x509_cert_url'] = auth_provider
-  key['client_x509_cert_url'] = client
 
-  with open('keys/mlops-crawler-firebase-adminsdk.json', 'w', encoding='utf-8') as k:
-    json.dump(key,k)
-  init_firebase()
-  init_model()
-  app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    key = {}
+    key["private_key_id"] = private_key_id
+    key["private_key"] = private_key
+    key["type"] = service_type
+    key["project_id"] = project_id
+    key["client_email"] = client_email
+    key["client_id"] = client_id
+    key["auth_uri"] = auth_uri
+    key["token_uri"] = token_uri
+    key["auth_provider_x509_cert_url"] = auth_provider
+    key["client_x509_cert_url"] = client
+
+    with open("keys/mlops-crawler-firebase-adminsdk.json", "w", encoding="utf-8") as k:
+        json.dump(key, k)
+    init_firebase()
+    init_model()
+    app.run(host="0.0.0.0", port="5000")
--- util.py
+++ util.py
@@ -1,20 +1,33 @@
+from typing import Dict, Tuple
+
 import torch
+import numpy as np
 
 from transformers import AutoModelForSequenceClassification, PreTrainedTokenizerFast
-from typing import Dict, Tuple
+
 
 # gpu 환경이 가능한지 체크
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 # 모델 및 토크나이저를 불러오는 함수
 def load_model(
     model_path: str,
 ) -> Tuple[AutoModelForSequenceClassification, PreTrainedTokenizerFast]:
     model = (
-        AutoModelForSequenceClassification.from_pretrained(model_path).to(device).eval()
+        AutoModelForSequenceClassification.from_pretrained(
+            model_path).to(device).eval()
     )
     tokenizer = PreTrainedTokenizerFast.from_pretrained(model_path)
     return model, tokenizer
+
+
+def softmax(x: np.array) -> np.array:
+    max = np.max(x, axis=1, keepdims=True)
+    e_x = np.exp(x - max)
+    sum = np.sum(e_x, axis=1, keepdims=True)
+    f_x = e_x / sum
+    return f_x
 
 
 # predict 결과를 Dict로 반환해주는 함수 1 : 긍정, 0 : 부정 score는 해당 클래스일 확률 값(0~1)
@@ -25,7 +38,7 @@ def predict(
     sequence: str,
 ) -> Dict:
     input_vector = tokenizer.encode(sequence, return_tensors="pt").to(device)
-    logit = model(input_ids=input_vector).logits
+    logit = softmax(model(input_ids=input_vector).logits)
     pred = logit.argmax(dim=-1).tolist()
     if pred[0] == 1:
         return {"label": "positive", "score": logit[0][1].item()}
